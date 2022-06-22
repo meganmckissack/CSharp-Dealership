0e4c930ccf8e1d0ca6fc3ee54cb089b306ad7673
Megan McKissack <meganmckissack@gmail.com>
2022-06-22 16:29:01 -0700

    project boilerplate and setup

---
+++ Models/Car.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Dealership.Models
+{
+  public class Car
+  {
+    private string MakeModel;
+    private int Price;
+    private int Miles;
+
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+    }
+
+    public bool WorthBuying(int maxPrice)
+    {
+      return (Price <= maxPrice);
+    }
+  }
+}
